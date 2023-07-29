d9e8dc96dc9d3490466d8499e0075913ff3c5102
fotso94 <richie@Richies-MacBook-Pro.local>
2023-07-29 18:48:27 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('AWS TETE WORLD...GM Mr Love....!'));
+app.get('/', (req, res) => res.send('AWS TETE WORLD...GM Mr Love....This is a Change of My Life!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
